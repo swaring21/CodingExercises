2fcdace956895e026405fd0afca80ba13e37bbb0
swaring21 <spwaring21@gmail.com>
2022-11-21 21:39:02 +1000

    Input Name
    You input your name.

---
+++ InputName/InputName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name!");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + "!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
