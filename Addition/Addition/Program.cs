ca920800232cd32bfa0a66953e6af8fd72ade5f5
swaring21 <spwaring21@gmail.com>
2022-11-22 21:33:22 +1000

    Add files via upload
    Addition and declaring variables.

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number, and we'll add it to the next number you type!");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Now type the next number to add to " + number + ".");
+
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            int numberSum = number + numberTwo;
+
+            Console.WriteLine("That Equals..." + numberSum + ".");
+
+            Console.ReadLine();
+
+        }
+    }
+}
