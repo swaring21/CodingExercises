f036b30b8507fbad951180c2b8b61ccda464fc01
swaring21 <spwaring21@gmail.com>
2022-11-26 18:00:34 +1000

    Add files via upload

---
+++ CompareOddsAndEvens/CompareOddsAndEvens/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareOddsAndEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            if (number % 2 == 0)
+
+                {
+
+                    Console.WriteLine("Your number is even!");
+
+                    Console.ReadLine();
+
+
+                }
+
+            else
+                {
+
+                Console.WriteLine("Your number is odd!");
+
+                Console.ReadLine();
+
+                }
+        }
+    }
+}
