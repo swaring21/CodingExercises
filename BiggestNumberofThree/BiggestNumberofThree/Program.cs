e8d657678f762eb6307bd5b43c98228803a87aed
swaring21 <spwaring21@gmail.com>
2022-12-18 14:54:02 +1000

    Add files via upload
    Input three numbers and find which one is the biggest.

---
+++ BiggestNumberofThree/BiggestNumberofThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggestNumberofThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your first number!");
+
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number!");
+
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number!");
+
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The biggest Number is" + Math.Max (Math.Max(firstNumber, secondNumber),thirdNumber));
+
+            Console.ReadLine();
+
+        }
+    }
+}
