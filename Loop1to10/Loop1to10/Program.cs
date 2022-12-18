c39d795a94eaf9d3e4de5614d73a2dd23b0685b4
swaring21 <spwaring21@gmail.com>
2022-12-18 11:46:43 +1000

    Add files via upload
    Printing an five line array of random numbers between 1 - 10.

---
+++ Loop1to10/Loop1to10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            int rndInt = rnd.Next(0, 10);
+
+            Console.WriteLine(rnd.Next(1, 10));
+
+            Console.WriteLine(rnd.Next(1, 10));
+
+            Console.WriteLine(rnd.Next(1, 10));
+
+            Console.WriteLine (rnd.Next(1, 10));
+
+            Console.WriteLine(rnd.Next(1, 10));
+
+            Console.ReadLine();
+
+        }
+    }
+}
